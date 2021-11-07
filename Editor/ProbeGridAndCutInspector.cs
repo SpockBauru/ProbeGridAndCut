b25f44e43a9989bc5f435d3afc23aa9cbf0a91ba
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2021-11-07 19:08:32 -0300

    Added Cut Probes Far from Objects and Only Static toogle

--- Editor/ProbeGridAndCutInspector.cs
+++ Editor/ProbeGridAndCutInspector.cs
@@ -17,7 +17,7 @@ public class ProbeGridAndCutInspector : Editor
         GUIStyle TextFieldStyles = new GUIStyle(EditorStyles.label);
 
         // ========================================Create Light Probe Grid Section========================================
-        EditorGUILayout.Space(20f);
+        //EditorGUILayout.Space(20f);
         EditorGUILayout.LabelField("Number of Light Probes on each axis", EditorStyles.boldLabel);
 
         Grid.probesInX = EditorGUILayout.IntField(new GUIContent("X", "Minimum is 2"), Grid.probesInX);
@@ -34,12 +34,21 @@ public class ProbeGridAndCutInspector : Editor
 
         if (GUILayout.Button("Generate Light Probes Grid"))
         {
-            if (ProbeCount <= 100000) Grid.Generate();
-            else _=EditorUtility.DisplayDialog("Aborting","ProbeGridAndCut Cannot handle more than 100,000 probes", "Ok");
+            if (ProbeCount <= 100000)
+            {
+                Grid.Generate();
+                Grid.UpdateProbes();
+            }
+            else _= EditorUtility.DisplayDialog("Aborting", "ProbeGridAndCut Cannot handle more than 100,000 probes", "Ok");
         }
 
+        //========================================Only cut on Static Objects========================================
+        //EditorGUILayout.Space(20f);
+        EditorGUILayout.LabelField("Check if you want to make cuts only on static objects", EditorStyles.boldLabel);
+        Grid.onlyStatic = EditorGUILayout.Toggle("Static Objects Only?", Grid.onlyStatic);
+
         //========================================Cut Probes on Tagged Boundaries Section========================================
-        EditorGUILayout.Space(20f);
+        //EditorGUILayout.Space(20f);
         EditorGUILayout.LabelField("Object Tags that mark the grid boundary", EditorStyles.boldLabel);
 
         for (int i = 0; i < Grid.BoundaryTags.Count; i++)
@@ -63,17 +72,49 @@ public class ProbeGridAndCutInspector : Editor
         if (GUILayout.Button("Cut Probes Outside Tagged Boundaries"))
         {
             Grid.CutTaggedObjects();
+            Grid.UpdateProbes();
         }
 
         //========================================Cut Probes on Objects Section========================================
-        EditorGUILayout.Space(20f);
-        EditorGUILayout.LabelField("Max Size of Objects That Cut Probes", EditorStyles.boldLabel);
+        //EditorGUILayout.Space(20f);
+        EditorGUILayout.LabelField("Size of rays (yellow lines) that test proximity with objects", EditorStyles.boldLabel);
 
-        Grid.ObjectMaxSize = EditorGUILayout.FloatField(new GUIContent("Object Max Size", "Make more than one object, but less than two"), Grid.ObjectMaxSize);
+        Grid.rayTestSize = EditorGUILayout.FloatField(new GUIContent("Ray test size", "Make more than one object, but less than two"), Grid.rayTestSize);
 
         if (GUILayout.Button("Cut Probes Inside Objects"))
         {
             Grid.CutInsideObjects();
+            Grid.UpdateProbes();
+        }
+        if (GUILayout.Button("Cut Probes Far From Objects"))
+        {
+            Grid.CutFarFromObject();
+            Grid.UpdateProbes();
+        }
+
+        //========================================Make Everything Section========================================
+        EditorGUILayout.Space(20f);
+        EditorGUILayout.LabelField("Make everything", EditorStyles.boldLabel);
+        EditorGUILayout.LabelField("Generate probes, cut bondaries, cut inside and cut outside");
+        //EditorGUILayout.LabelField("cut inside objects and cut far from objects");
+
+        var style = new GUIStyle(GUI.skin.button);
+        style.normal.textColor = Color.white;
+        style.fontStyle = FontStyle.Bold;
+        style.hover.textColor = Color.white;
+        style.active.textColor = Color.white;
+        GUI.backgroundColor= Color.red;
+        if (GUILayout.Button("Make Everything", style))
+        {
+            Grid.Generate();
+            Grid.CutTaggedObjects();
+            Grid.CutInsideObjects();
+            Grid.CutFarFromObject();
+
+            //Display as warning if number of probes is too big
+            if (Grid.probePositions.Count<=100000)
+                Grid.UpdateProbes();
+            else _ = EditorUtility.DisplayDialog("Aborting", "ProbeGridAndCut Cannot handle more than 100,000 probes", "Ok");
         }
     }
 }
--- Script/ProbeGridAndCut.cs
+++ Script/ProbeGridAndCut.cs
@@ -15,23 +15,17 @@ public class ProbeGridAndCut : MonoBehaviour
     [HideInInspector]
     public int probesInZ = 5;
 
-    //Max size of raycast from each probe
+    //check if only static objects will be tested
     [HideInInspector]
-    public float ObjectMaxSize = 1f;
+    public bool onlyStatic = true;
 
     //List of tags that will be tested in raycastAll
     [HideInInspector]
     public List<string> BoundaryTags = new List<string> { "Empty      " };
 
-    //Position relative to parent, between 0 and 1
-    private Vector3 position;
-    private Vector3 startPosition;
-
-
-    //Lenght relative to parent, between 0 and 1
-    private float stepX;
-    private float stepY;
-    private float stepZ;
+    //Max size of raycast from each probe
+    [HideInInspector]
+    public float rayTestSize = 1f;
 
     LightProbeGroup probeGroup;
     [HideInInspector]
@@ -43,6 +37,23 @@ public class ProbeGridAndCut : MonoBehaviour
         probePositions = new List<Vector3>(probeGroup.probePositions);
     }
 
+    private void Update()
+    {
+        Box.center = transform.position;
+        Box.extents = transform.localScale;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Box.center, Box.extents);
+    }
+
+    public void UpdateProbes()
+    {
+        //Update Light Probe Group in Editor
+        probeGroup.probePositions = probePositions.ToArray();
+    }
+
     public void Generate()
     {
         probePositions.Clear();
@@ -52,13 +63,16 @@ public class ProbeGridAndCut : MonoBehaviour
         if (probesInY < 2) probesInY = 2;
         if (probesInZ < 2) probesInZ = 2;
 
+        //Position relative to parent, between 0 and 1
+        Vector3 position;
+
         //Calculating steps on each exis, lenght between 0 and 1
-        stepX = 1f / (probesInX - 1);
-        stepY = 1f / (probesInY - 1);
-        stepZ = 1f / (probesInZ - 1);
+        float stepX = 1f / (probesInX - 1);
+        float stepY = 1f / (probesInY - 1);
+        float stepZ = 1f / (probesInZ - 1);
 
         //Start position relative to the parent center. (Relative parent size is always 1)
-        startPosition.Set(-0.5f, -0.5f, -0.5f);
+        Vector3 startPosition = new Vector3(-0.5f, -0.5f, -0.5f);
 
         //Recursive probe position table
         for (int x = 0; x < probesInX; x++)
@@ -74,9 +88,6 @@ public class ProbeGridAndCut : MonoBehaviour
                 }
             }
         }
-
-        //Place probes on all positions
-        probeGroup.probePositions = probePositions.ToArray();
     }
 
     public void CutTaggedObjects()
@@ -89,8 +100,6 @@ public class ProbeGridAndCut : MonoBehaviour
 
         RaycastHit[] hitAll;
 
-        //List<string> tags = new List<string>(BoundaryTags);
-
         for (int i = probePositions.Count - 1; i >= 0; i--)
         {
             //scaling from relative to world position
@@ -102,31 +111,32 @@ public class ProbeGridAndCut : MonoBehaviour
             direction = position - center;
 
             //trace a raycast from the center to the probe. If the hit a tagged object the probe is removed from the list.
+            Debug.DrawLine(center, position, Color.yellow, 1);
             hitAll = Physics.RaycastAll(center, direction, distance);
             for (int j = 0; j < hitAll.Length; j++)
             {
-                if (BoundaryTags.Contains(hitAll[j].transform.gameObject.tag))
+                if ((BoundaryTags.Contains(hitAll[j].transform.gameObject.tag) &&
+                    (hitAll[j].collider.gameObject.isStatic || !onlyStatic)))
                 {
                     probePositions.RemoveAt(i);
                     break;
                 }
             }
         }
-
-        //Update Light Probe Group
-        probeGroup.probePositions = probePositions.ToArray();
     }
 
     public void CutInsideObjects()
     {
         Vector3 center = transform.position;
         Vector3 scale = transform.localScale;
-        Vector3 position, rayPos=Vector3.zero;
+        Vector3 position;
+        Vector3 rayPos = Vector3.zero;
 
-        string name1, name3, name4, name5, name6;
+        string name1, name2, name3, name4, name5;
 
         RaycastHit hit;
 
+        //Raycast all 5 sides from selected size to the probe. If all directions have the same name, the probe is inside an object.
         for (int i = probePositions.Count - 1; i >= 0; i--)
         {
             //scaling from relative to world position
@@ -134,51 +144,128 @@ public class ProbeGridAndCut : MonoBehaviour
             position += center;
 
             name1 = "1";
+            name2 = "2";
             name3 = "3";
             name4 = "4";
             name5 = "5";
-            name6 = "6";
-
-            //Raycast all 6 sides from the box boudaries to the probe. if all directions have the same name, the probe is inside an object.
-            rayPos.Set(position.x, position.y + ObjectMaxSize, position.z);
+            
+            //Up to probe
+            rayPos.Set(position.x, position.y + rayTestSize, position.z);
             if (Physics.Raycast(rayPos, Vector3.down, out hit, Vector3.Distance(rayPos, position))) name1 = hit.transform.name;
             Debug.DrawLine(position, rayPos, Color.yellow, 1);
 
-            rayPos.Set(position.x + ObjectMaxSize, position.y, position.z);
-            if (Physics.Raycast(rayPos, Vector3.left, out hit, Vector3.Distance(rayPos, position))) name3 = hit.transform.name;
+            //Right to Probe
+            rayPos.Set(position.x + rayTestSize, position.y, position.z);
+            if (Physics.Raycast(rayPos, Vector3.left, out hit, Vector3.Distance(rayPos, position))) name2 = hit.transform.name;
             Debug.DrawLine(position, rayPos, Color.yellow, 1);
 
-            rayPos.Set(position.x - ObjectMaxSize, position.y, position.z);
-            if (Physics.Raycast(rayPos, Vector3.right, out hit, Vector3.Distance(rayPos, position))) name4 = hit.transform.name;
+            //Left to Probe
+            rayPos.Set(position.x - rayTestSize, position.y, position.z);
+            if (Physics.Raycast(rayPos, Vector3.right, out hit, Vector3.Distance(rayPos, position))) name3 = hit.transform.name;
             Debug.DrawLine(position, rayPos, Color.yellow, 1);
 
-            rayPos.Set(position.x, position.y, position.z + ObjectMaxSize);
-            if (Physics.Raycast(rayPos, Vector3.back, out hit, Vector3.Distance(rayPos, position))) name5 = hit.transform.name;
+            //Forward to Probe
+            rayPos.Set(position.x, position.y, position.z + rayTestSize);
+            if (Physics.Raycast(rayPos, Vector3.back, out hit, Vector3.Distance(rayPos, position))) name4 = hit.transform.name;
             Debug.DrawLine(position, rayPos, Color.yellow, 1);
 
-            rayPos.Set(position.x, position.y, position.z - ObjectMaxSize);
-            if (Physics.Raycast(rayPos, Vector3.forward, out hit, Vector3.Distance(rayPos, position))) name6 = hit.transform.name;
+            //Back to Probe
+            rayPos.Set(position.x, position.y, position.z - rayTestSize);
+            if (Physics.Raycast(rayPos, Vector3.forward, out hit, Vector3.Distance(rayPos, position))) name5 = hit.transform.name;
             Debug.DrawLine(position, rayPos, Color.yellow, 1);
 
-            if (name1 == name3 &&
+            if (name1 == name2 &&
+                name1 == name3 &&
                 name1 == name4 &&
                 name1 == name5 &&
-                name1 == name6) probePositions.RemoveAt(i);
+                (hit.collider.gameObject.isStatic || !onlyStatic))
+                probePositions.RemoveAt(i);
         }
-
-        //Update Light Probe Group
-        probeGroup.probePositions = probePositions.ToArray();
     }
 
-    private void OnDrawGizmosSelected()
+    public void CutFarFromObject()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(Box.center, Box.extents);
-    }
+        Vector3 center = transform.position;
+        Vector3 scale = transform.localScale;
+        Vector3 position;
+        Vector3 endLine = Vector3.zero;
 
-    private void Update()
-    {
-        Box.center = transform.position;
-        Box.extents = transform.localScale;
+        bool hitObject;
+        bool hitTest;
+
+        RaycastHit hit;
+
+        //Raycast all 6 sides from selected size to the probe, and from the probe to the selected size.
+        //If there's at least one hit, the box is near an object and will not be cut
+        for (int i = probePositions.Count - 1; i >= 0; i--)
+        {
+            //scaling from relative to world position
+            position = Vector3.Scale(probePositions[i], scale);
+            position += center;
+
+            hitObject = false;
+
+            //Up to probe and probe to Up
+            endLine.Set(position.x, position.y + rayTestSize, position.z);
+            hitTest = Physics.Raycast(endLine, Vector3.down, out hit, Vector3.Distance(endLine, position));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            hitTest = Physics.Raycast(position, Vector3.up, out hit, Vector3.Distance(position, endLine));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            Debug.DrawLine(position, endLine, Color.yellow, 1);
+
+            //Bottom to probe and probe to Bottom
+            endLine.Set(position.x, position.y - rayTestSize, position.z);
+            hitTest = Physics.Raycast(endLine, Vector3.up, out hit, Vector3.Distance(endLine, position));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            hitTest = Physics.Raycast(position, Vector3.down, out hit, Vector3.Distance(position, endLine));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            Debug.DrawLine(position, endLine, Color.yellow, 1);
+
+            //Right to probe and probe to Right
+            endLine.Set(position.x + rayTestSize, position.y, position.z);
+            hitTest = Physics.Raycast(endLine, Vector3.left, out hit, Vector3.Distance(endLine, position));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            hitTest = Physics.Raycast(position, Vector3.right, out hit, Vector3.Distance(position, endLine));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            Debug.DrawLine(position, endLine, Color.yellow, 1);
+
+            //Left to probe and probe to Left
+            endLine.Set(position.x - rayTestSize, position.y, position.z);
+            hitTest = Physics.Raycast(endLine, Vector3.right, out hit, Vector3.Distance(endLine, position));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            hitTest = Physics.Raycast(position, Vector3.left, out hit, Vector3.Distance(position, endLine));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            Debug.DrawLine(position, endLine, Color.yellow, 1);
+
+            //Forward to probe and probe to Forward
+            endLine.Set(position.x, position.y, position.z + rayTestSize);
+            hitTest = Physics.Raycast(endLine, Vector3.back, out hit, Vector3.Distance(endLine, position));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            hitTest = Physics.Raycast(position, Vector3.forward, out hit, Vector3.Distance(position, endLine));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            Debug.DrawLine(position, endLine, Color.yellow, 1);
+
+            //Back to probe and probe to Back
+            endLine.Set(position.x, position.y, position.z - rayTestSize);
+            hitTest = Physics.Raycast(endLine, Vector3.forward, out hit, Vector3.Distance(endLine, position));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            hitTest = Physics.Raycast(position, Vector3.back, out hit, Vector3.Distance(position, endLine));
+            hitObject = hitObject || (hitTest && (hit.collider.gameObject.isStatic || !onlyStatic));
+
+            Debug.DrawLine(position, endLine, Color.yellow, 1);
+
+            if (!hitObject) probePositions.RemoveAt(i);
+        }
     }
 }
