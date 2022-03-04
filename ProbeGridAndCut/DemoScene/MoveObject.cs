7d508c0f59c28c5020e196948488c3299e9a318e
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2022-03-04 14:08:37 -0300

    Updated Demo Scene

---
+++ ProbeGridAndCut/DemoScene/MoveObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+
+    Vector3 position;
+
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x = position.x - Time.deltaTime;
+        if (position.x < -1.5f) position.x = 2.5f;
+        transform.position = position;
+    }
+}
